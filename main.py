5d51d12f8bbfe20097d753c3a4ac53d459a43808
KubaPrzydzial <jjakub.przydzial@gmail.com>
2023-02-02 16:55:50 +0100

    fixed issues / performance improvements

--- main.py
+++ main.py
@@ -7,72 +7,74 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import csv
 import time
+# from datetime import datetime
 
-# open the file with brand names
-with open("brandy.txt", "r") as file:
-    brands = file.readlines()
+# start_time = datetime.now()
 
-# create webdriver
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 driver.maximize_window()
-
-# go to website
 driver.get("url")
 
+# open the file with brand names
+with open("brandy.txt", "r", encoding='utf-8') as file:
+    brands = file.readlines()
+    # Loop over each brand
+    for brand in brands:
+        # accept cookies
+        try:
+            element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Tak, zgadzam się')]"))).click()
+        except:
+        # if not found just carry on
+            pass
 
-# Loop over each brand
-for brand in brands:
-    brand = brand.strip()
+        try:
+            # pick category
+            element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Męskie')]"))).click()
+        except:
+            pass
 
-    # accept cookies
-    try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Tak, zgadzam się')]"))).click()
-    except:
-    # if not found just carry on
-        pass
-
-    try:
-        # pick category
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Męskie')]"))).click()
-    except:
-        pass
-
-    # find search field and input brand name
-    try:
         search_field = WebDriverWait(driver, 5).until(EC.presence_of_element_located(("id", "brands-autocomplete")))
-        search_field.send_keys(brand)
-        time.sleep(3)
-        search_field.send_keys(Keys.RETURN)
-    except:
-        # if not found, skip to next brand
         search_field.clear()
+        search_field.send_keys(brand)
 
-    try:
-        button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Dalej')]"))).click()
-    except:
-        continue
+        link = None
+        try:
+            link = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, ".//*[@id='brands-autocomplete-menu-item-0']")))
+        except:
+            # if not found, go back to start of the loop
+            continue
 
-    try:
-        link = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, ".//*[@id='category-54']"))).click()
-        time.sleep(3)
-    finally:
-        link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'Kurtka (skóra naturalna)')]")))
-        link.click()
+        if link:
+            link.click()
+            try:
+                button = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Dalej')]"))).click()
+            except:
+                continue
 
-    try:
-        # locate the elements to be saved
-        brand_id_element = driver.find_element("id", "brand-crumb")
-        brand_name = brand_id_element.get_attribute("data-gtm-brand") ### brand name
-        brand_id = brand_id_element.get_attribute("data-gtm-brand-id") ### brand id
-        price_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@class, 'price py-4')]")))  ### locate price element
-        price = price_element.text ### price
-        # write the brand, brand_id, and price to a CSV file
-        with open("output.csv", "a", newline="", encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerow([brand_name, brand_id, price])
+        try:
+            link = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, ".//*[@id='category-54']"))).click()
+            # time.sleep(3)
+        finally:
+            link = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(),'Kurtka (skóra naturalna)')]")))
+            link.click()
 
-    finally:
-        driver.get("url")
+        try:
+            # locate the elements to be saved
+            brand_id_element = driver.find_element("id", "brand-crumb")
+            brand_name = brand_id_element.get_attribute("data-gtm-brand") ### brand name
+            brand_id = brand_id_element.get_attribute("data-gtm-brand-id") ### brand id
+            price_element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@class, 'price py-4')]")))  ### locate price element
+            price = price_element.text ### price
+            price_replaced = price.replace('zł', 'PLN')
+            # write the brand, brand_id, and price to a CSV file
+            with open("output.csv", "a", newline="", encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow([brand_name, brand_id, price_replaced])
 
+        finally:
+            driver.get("url")
+
+# end_time = datetime.now()
+# print('Duration: {}'.format(end_time - start_time))
 # Close the WebDriver
 driver.close()
